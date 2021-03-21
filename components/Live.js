a2caec3b8784da6a80a748f87cb876c3ce1f029a
salvatore.pandolfi <salvpan@gmail.com>
2021-03-21 17:52:07 +0100

    Added Entry Detail Reset function Ui Component completed

--- components/Live.js
+++ components/Live.js
@@ -2,18 +2,72 @@ import React, {Component} from 'react'
 import {View, Text, ActivityIndicator, StyleSheet, TouchableOpacity} from "react-native";
 import {purple, white} from "../utils/colors";
 import {Foundation} from '@expo/vector-icons'
+import * as Permissions from "expo-permissions";
+import * as Location from "expo-location";
+import {calculateDirection} from "../utils/helpers";
+
 
 export default class Live extends Component {
 
     state = {
         coords: null,
-        status: 'granted',
+        status: 'undetermined',
         direction: ''
     }
 
-    askPermissions = () => {
-        return 'hello'
+
+    componentDidMount() {
+        Location.requestPermissionsAsync()
+            .then(({status}) => {
+                if (status === 'granted') {
+                    this.setLocation()
+                }
+                this.setState(() => {
+                    status
+                })
+            })
+            .catch((error) => {
+                console.warn('Error getting location permissions: ', error)
+                this.setState(() => {
+                    state:'undetermined'
+                })
+            })
+
     }
+
+    askPermissions = () => {
+        Permissions.askAsync(Permissions.location)
+            .then(({status}) => {
+                if (status === 'granted') {
+                    return this.setLocation()
+                }
+                this.setState(() => {
+                    status
+                })
+            })
+            .catch((error) => {
+                console.warn('Error getting location permissions: ', error)
+                this.setState(() => {
+                    state:'undetermined'
+                })
+            })
+    }
+    setLocation = () => {
+        Location.watchPositionAsync({
+            enableHighAccuracy: true,
+            timeInterval: 1,
+            distanceInterval: 1
+        }, ({coords}) => {
+            const newDirection = calculateDirection(coords.heading)
+            const {direction} = this.state
+            this.setState(() => ({
+                coords,
+                status: 'granted',
+                direction: newDirection
+            }))
+        })
+    }
+
 
     render() {
         const {status, coords, direction} = this.state
@@ -53,7 +107,7 @@ export default class Live extends Component {
             <View style={styles.container}>
                 <View style={styles.directionContainer}>
                     <Text style={styles.header}>You're heading</Text>
-                    <Text style={styles.direction}>North</Text>
+                    <Text style={styles.direction}>{direction}</Text>
                 </View>
                 <View style={styles.metricContainer}>
                     <View style={styles.metric}>
@@ -61,7 +115,7 @@ export default class Live extends Component {
                             Altitude
                         </Text>
                         <Text style={[styles.subHeader, {color: white}]}>
-                            {200} Feet
+                            {Math.round(coords.altitude)} METERS
                         </Text>
                     </View>
                     <View style={styles.metric}>
@@ -69,7 +123,7 @@ export default class Live extends Component {
                             Speed
                         </Text>
                         <Text style={[styles.subHeader, {color: white}]}>
-                            {300} MPH
+                            {Math.round(coords.speed)} KPH
                         </Text>
                     </View>
                 </View>
