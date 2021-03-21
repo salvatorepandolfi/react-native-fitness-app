5cb02ac73c2734b1e0aeeb7dad3719a3b73c4afc
salvatore.pandolfi <salvpan@gmail.com>
2021-03-21 11:56:51 +0100

    Added Entry Detail Component

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react'
-import {View, Text} from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import {connect} from 'react-redux'
+import MetricCard from "./MetricCard";
 
-class EntryDetail extends Component{
+class EntryDetail extends Component {
+    componentDidMount() {
+        this.setTitle(this.props.route.params.entryId);
+    }
+
+    setTitle = (entryId) => {
+        if (!entryId) return;
+        const year = entryId.slice(0, 4)
+        const month = entryId.slice(5, 7)
+        const day = entryId.slice(8)
+        this.props.navigation.setOptions({title: `${month}/${day}/${year}`});
+    };
+
     render() {
-        // console.log(this.props)
-        return(
-            <View>
+        const {metrics} = this.props
+        return (
+            <View style={styles.container}>
+                <MetricCard metrics={metrics}/>
                 <Text>Entry Detail - {JSON.stringify(this.props.route.params.entryId)}</Text>
             </View>
         )
     }
 }
 
-export default EntryDetail
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 15
+    }
+})
+
+const mapStateToProps = (state, {route}) => {
+    const entryId = route.params.entryId
+    const [metrics] = state[entryId]
+    return {
+        entryId,
+        metrics: metrics
+    }
+}
+export default connect(mapStateToProps)(EntryDetail)
